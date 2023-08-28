b1f8d490b95c8735693fc5b473b66696f17825a1
Amanda <windsoramanda@outlook.com>
2023-08-28 17:47:22 +1000

    Initial commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Amanda Windsor-POOP The Party-1.1";
+const contentToCache = [
+    "Build/webglptpV2.loader.js",
+    "Build/webglptpV2.framework.js.unityweb",
+    "Build/webglptpV2.data.unityweb",
+    "Build/webglptpV2.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
